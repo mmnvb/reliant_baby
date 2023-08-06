e47edb6742a53e725c7f18f5004dca5d0fd306b8
muminovbob <bobomurodm2004@gmail.com>
2023-08-06 12:07:08 +0500

    Add voice message normalization

--- bot_brain/misc/convert_audio.py
+++ bot_brain/misc/convert_audio.py
@@ -12,9 +12,14 @@ async def m4a_to_mp3(file: int, author: str, title: str):
 
 
 async def ogg_to_mp3(file: int, author: str):
+    def match_target_amplitude(sound_self, target_dbfs):
+        change_in_dBFS = target_dbfs - sound.dBFS
+        return sound_self.apply_gain(change_in_dBFS)
+
     sound = AudioSegment.from_file(f"temp/{file}.ogg")
-    sound.export(f"temp/{file}.mp3", format="mp3",
-                 tags={'artist': f"{author}",
-                       'album': 'ReliantBaby',
-                       'title': f"audio"})
+    normalized = match_target_amplitude(sound, -20.0)
+    normalized.export(f"temp/{file}.mp3", format="mp3",
+                      tags={'artist': f"{author}",
+                            'album': 'ReliantBaby',
+                            'title': f"audio"})
     remove(f"temp/{file}.ogg")
